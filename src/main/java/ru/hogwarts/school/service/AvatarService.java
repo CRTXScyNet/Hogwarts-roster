821573cd169bb433d7f1f4990eb9788b1548cb6b
CRTXScyNet <CRTXScyNet@gmail.com>
2024-07-19 20:20:11 +0500

    Logging and configurating

--- src/main/java/ru/hogwarts/school/service/AvatarService.java
+++ src/main/java/ru/hogwarts/school/service/AvatarService.java
@@ -1,6 +1,8 @@
 package ru.hogwarts.school.service;
 
 import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -19,7 +21,7 @@ import java.util.Collection;
 @Service
 @Transactional
 public class AvatarService {
-
+    private final Logger logger = LoggerFactory.getLogger(AvatarService.class);
     @Value("${student.avatar.dir.path}")
     private String avatarsDir;
     private AvatarRepository avatarRepository;
@@ -31,6 +33,7 @@ public class AvatarService {
     }
 
     public void uploadAvatar(Long studentId, MultipartFile file) throws IOException {
+        logger.info("Был вызван метод 'uploadAvatar'");
         Student student = studentService.getStudent(studentId);
 
         Path filePath = Path.of(avatarsDir, studentId + "." + getExtension(file.getOriginalFilename()));
@@ -51,20 +54,24 @@ public class AvatarService {
     }
 
     public Avatar findAvatar(Long studentId) {
+        logger.info("Был вызван метод 'findAvatar'");
         return avatarRepository.findByStudentId(studentId).orElse(null);
     }
 
     private Avatar getAvatar(Long studentId) {
+        logger.info("Был вызван метод 'getAvatar'");
         return avatarRepository.findByStudentId(studentId).orElse(new Avatar());
     }
 
     private String getExtension(String s) {
+        logger.info("Был вызван метод 'getExtension'");
         int dot = s.lastIndexOf('.');
         return s.substring(dot + 1);
     }
 
     public Collection<Avatar> getAvatars(Integer pageNumber, Integer pageSize) {
-        PageRequest request = PageRequest.of(pageNumber,pageSize);
+        logger.info("Был вызван метод 'getAvatars'");
+        PageRequest request = PageRequest.of(pageNumber, pageSize);
         return avatarRepository.findAll(request).getContent();
     }
 }
